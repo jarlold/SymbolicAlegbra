28d448b342df7c85c2118273850d659cdf2170f8
User <jarlold@protonmail.com>
2025-08-14 22:38:38 -0400

    Moved losses to be in neural networks, even though they technically have other uses...

--- neural.cpp
+++ neural.cpp
@@ -5,11 +5,10 @@
 #include "node_calculus.cpp"
 #include "common.cpp"
 
-
 // Requires that the user call backpropagation first
 void updateWeightsMatrix(Matrix m, float lr) {
-    int l = m.size();
-    int w = m[0].size();
+    int w = m.size();
+    int l = m[0].size();
 
     for (int i=0; i < w; i++) {
         for (int j=0; j<l; j++) {
@@ -19,7 +18,41 @@ void updateWeightsMatrix(Matrix m, float lr) {
 
 }
 
+valueMatrix xavierInitMatrix(size_t width, size_t height);
 
-void xavierInitializeMatrix(Matrix m, int depth);
+NodePtr meanSquaredError(Vector& predictions, Vector& groundTruth) {
+    NodePtr loss = constantNode(0);
+    NodePtr two = constantNode(2); // dont judge me
+    int len = groundTruth.size();
+    NodePtr lenN = constantNode(len);
+
+    for (int i=0; i<len; i++) {
+        NodePtr iloss = powNode(addNodes(predictions[i], negNode(groundTruth[i])), two);
+        loss = addNodes(loss, iloss);
+    }
+
+    loss = divNodes(loss,lenN);
+    return loss;
+}
+
+Vector softmax(Vector& logits) {
+    Vector probs(logits.size());
+
+    // S = sum e^x_i
+    NodePtr sum = constantNode(0);
+    for (size_t i=0; i<logits.size(); i++) {
+        sum = addNodes(sum, expNode(logits[i]));
+    }
+
+    // Probs_i = x_i / S 
+    for (size_t i=0; i<logits.size(); i++) {
+        probs[i] = expNode(logits[i]);
+    }
+
+    return probs;
+}
 
 
+NodePtr crossEntropy(NodePtr guessedProb) {
+    return negNode(logNode(guessedProb));
+}
